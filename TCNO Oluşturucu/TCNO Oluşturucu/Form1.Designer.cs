57fb67e72caa8ca1ec4a872a4ca981eb4862ffc1
Göksel Sina <48367016+skyflod@users.noreply.github.com>
2019-03-28 02:55:20 +0300

    Add files via upload

---
+++ TCNO Oluşturucu/TCNO Oluşturucu/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TCNO_Oluşturucu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoEllipsis = true;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(77, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OLUŞTUR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(77, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.AutoEllipsis = true;
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(77, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "KONTROL";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(74, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Kontrol Sonucu : ---";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(309, 203);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TCNO Oluşturucu/TCNO Oluşturucu/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCNO_Oluşturucu
+{
+    public partial class Form1 : Form
+    {
+        int b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random bsmk = new Random();
+           
+            b1 = bsmk.Next(1,10);
+            b2 = bsmk.Next(0,10);
+            b3 = bsmk.Next(0,10);
+            b4 = bsmk.Next(0,10);
+            b5 = bsmk.Next(0,10);
+            b6 = bsmk.Next(0,10);
+            b7 = bsmk.Next(0,10);
+            b8 = bsmk.Next(0,10);
+            b9 = bsmk.Next(0,10);
+            b10 = (((b1 + b3 + b5 + b7 + b9) * 7) - (b2 + b4 + b6 + b8)) % 10;
+            b11 = (b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8 + b9 + b10) % 10;
+
+            textBox1.Text = b1.ToString() + b2.ToString() + b3.ToString() + b4.ToString() + b5.ToString() +b6.ToString() + b7.ToString() + b8.ToString() + b9.ToString() + b10.ToString() + b11.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int kural1;
+            int kural2;
+            
+            kural1 = ((b1 + b3 + b5 + b7 + b9) * 7) - (b2 + b4 + b6 + b8);
+            kural2 = b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8 + b9 + b10;
+            
+            if (kural1 % 10 == b10)
+            {
+                if (kural2 % 10 == b11)
+                {
+                    if (b1 == 0)
+                    {
+                        label1.Text = "Kontrol Sonucu : Hatalı ";
+                    }
+                    else
+                    {
+                       if (b11 % 2 == 1)
+                       {
+                           label1.Text = "Kontrol Sonucu : Hatalı ";
+                       }
+                       else
+                       {
+                           label1.Text = "Kontrol Sonucu : Doğru ";
+                       }
+                    }
+                }
+                else
+                {
+                    label1.Text = "Kontrol Sonucu : Hatalı ";
+                }
+
+            }
+            else
+            {
+                label1.Text = "Kontrol Sonucu : Hatalı ";
+            }
+        }
+    }
+}
